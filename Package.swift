a8b1d9c8f2035019738bc33c90eaed22f5070526
emelent <roy.motloutsi@gmail.com>
2025-03-23 00:03:43 +0200

    update package

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "plain",
-         url: "https://github.com/emelent/plain-framework/releases/download/0.0.4/plain.xcframework.zip",
-         checksum:"008ae2b5da8ff726c874ecde6d753210429b1fef5d793942f9c01a83a23f28dc")
+         url: "https://github.com/emelent/plain-framework/releases/download/0.0.5/plain.xcframework.zip",
+         checksum:"df872cb78b2f346d8f517078d24be580c3bc32390f203620b6e9227f75097913")
    ]
 )
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,13 @@
 import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 
 // build.gradle.kts
-plugins {
-    kotlin("multiplatform") version "2.1.20"
-}
+plugins { kotlin("multiplatform") version "2.1.20" }
 
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
 
 kotlin {
-    val buildNumber = 4
-    val version = "0.0.4"
+    val buildNumber = 5
+    val version = "0.0.5"
     val name = "plain"
     val xcf = XCFramework(name)
 
@@ -20,17 +16,17 @@ kotlin {
         iosArm64(),
         iosSimulatorArm64(),
         macosX64(),
-    ).forEach {
-        it.binaries.framework {
-            baseName = name
-            binaryOption("bundleId", "io.github.emelent.plain")
-            binaryOption("bundleVersion", "$buildNumber")
-            binaryOption("bundleShortVersionString", version)
-            isStatic = true
-            xcf.add(this)
-
+    )
+        .forEach {
+            it.binaries.framework {
+                baseName = name
+                binaryOption("bundleId", "io.github.emelent.plain")
+                binaryOption("bundleVersion", "$buildNumber")
+                binaryOption("bundleShortVersionString", version)
+                isStatic = true
+                xcf.add(this)
+            }
         }
-    }
 }
 
 tasks.withType<Wrapper> {
